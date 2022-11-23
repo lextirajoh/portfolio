17a36ad02d253524880720a6539edaf61feb7ebe
Lex <alext@disroot.org>
2022-11-24 00:08:33 +0100

    Setup vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-
 export default defineConfig({
-  site: 'https://lextirajoh.github.io',
-  base: '/portfolio',
-})
+  output: 'server',
+  adapter: vercel()
+});
